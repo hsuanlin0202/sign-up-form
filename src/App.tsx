44f9d617b89099743b49e727f035bf2b853a867d
sherin.lin <sherin.lin@car-plus.com.tw>
2021-08-18 16:05:37 +0800

    postMessage test

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,25 @@ import WordingSection from "./components/WordingSection";
 import FormSection from "./components/FormSection";
 
 const App = (): JSX.Element => {
+  const post = () => {
+    const message = JSON.stringify({
+      error: false,
+      message: "Hello World",
+    });
+
+    window?.top?.postMessage(message, "*");
+  };
+  console.log(window.top);
   return (
-    <div className="w-full min-h-screen bg-image bg-red-default px-6 py-20 md:py-0 flex flex-col md:flex-row justify-center items-center space-x-0 md:space-x-11">
-      <WordingSection />
-      <FormSection />
+    <div>
+      <button className="bg-red-default p-6" onClick={() => post()}>
+        Click
+      </button>
     </div>
+    // <div className="w-full min-h-screen bg-image bg-red-default px-6 py-20 md:py-0 flex flex-col md:flex-row justify-center items-center space-x-0 md:space-x-11">
+    //   {/* <WordingSection />
+    //   <FormSection /> */}
+    // </div>
   );
 };
 
--- src/components/FormSection/index.tsx
+++ src/components/FormSection/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import clsx from "clsx";
 import { Button } from "../widgets";
 import Form from "./Form";
